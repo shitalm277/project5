e0aa697f490f615911668f7cd3dfd3d5bbca6ed7
shitalm277 <shitalmehta277@gmail.com>
2021-10-17 17:38:39 +0100

    project5

---
+++ src/Project5.java
@@ -0,0 +1,9 @@
+public class Project5 {
+    public static void main(String[]args){
+        System.out.println("i like git");
+        System.out.println("i like git");
+        System.out.println("i like git");
+        System.out.println("i like git");
+        System.out.println("i like git");
+    }
+}
